f965e587e129ec14d225ce03e1f79331b3007e90
manojkumar24-cloud <68296548+manojkumar24-cloud@users.noreply.github.com>
2020-07-18 21:25:44 +0530

    Update demo2.java

--- src/Demo/demo2.java
+++ src/Demo/demo2.java
@@ -5,6 +5,8 @@ public class demo2 {
 	public static void main(String[] args) {
 		
 		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
 		// TODO Auto-generated method stub
 
 	}
